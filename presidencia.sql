2593a87fca877e5c564968793ce96140a331bd24
JoseDavidEsquivel <122810644+JoseDavidEsquivel@users.noreply.github.com>
2024-05-27 10:09:27 -0600

    Actualizacion de la api

--- presidencia.sql
+++ presidencia.sql
@@ -1,166 +1,472 @@
-CREATE DATABASE presidencia;
-USE presidencia;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-05-2024 a las 18:06:22
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- USUARIOS (DONE) -------------------------------------------------------
-CREATE TABLE usuarios (
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL UNIQUE,
-    contrasena VARCHAR(255) NOT NULL,
-    area VARCHAR(70) NOT NULL,
-    estado ENUM('0', '1') DEFAULT '1',
-    permisos ENUM('0', '1') DEFAULT '0',
-    salt VARCHAR(255) NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- PAGINA ---------------------------------------------------------
-CREATE TABLE logo (
-    id_logo INT AUTO_INCREMENT PRIMARY KEY,
-    imagen VARCHAR(255) NOT NULL,
-    ruta VARCHAR(255) NOT NULL
-);
+--
+-- Base de datos: `presidencia`
+--
 
-INSERT INTO logo (imagen,ruta) VALUES
-('default_icon.png','static/images/logo/');
+-- --------------------------------------------------------
 
-CREATE TABLE header (
-    id_header INT AUTO_INCREMENT PRIMARY KEY,
-    imagen VARCHAR(255) NOT NULL,
-    ruta VARCHAR(255) NOT NULL
-);
+--
+-- Estructura de tabla para la tabla `bot`
+--
 
-INSERT INTO header (imagen,ruta) VALUES
-('default_header.png','static/images/header/');
+CREATE TABLE `bot` (
+  `id` int(11) NOT NULL,
+  `nombre` text DEFAULT NULL,
+  `correo` text DEFAULT NULL,
+  `problema` text DEFAULT NULL,
+  `area` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE carrusel (
-    id_imagen INT AUTO_INCREMENT PRIMARY KEY,
-    imagen VARCHAR(255) NOT NULL,
-    ruta VARCHAR(255) NOT NULL,
-    estado ENUM('0', '1') DEFAULT '1',
-    url VARCHAR(255) NOT NULL
-);
+-- --------------------------------------------------------
 
-CREATE TABLE ubicaciones (
-    id_ubicacion INT AUTO_INCREMENT PRIMARY KEY,
-    latitud DECIMAL(10, 8),
-    longitud DECIMAL(11, 8),
-    lugar VARCHAR(255)
-);
+--
+-- Estructura de tabla para la tabla `carrusel`
+--
 
-CREATE TABLE contactos (
-    id_contactos INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_institucion TEXT,
-    tipo_contacto ENUM('telefono','email'),
-    contacto TEXT,
-    horario TEXT
-);
+CREATE TABLE `carrusel` (
+  `id_imagen` int(11) NOT NULL,
+  `imagen` varchar(255) NOT NULL,
+  `ruta` varchar(255) NOT NULL,
+  `estado` enum('0','1') DEFAULT '1',
+  `url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- NOTICIAS ---------------------------------------------------------
-CREATE TABLE noticias (
-    id_noticia INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(100),
-    resumen TEXT,
-    contenido longtext,
-    imagen VARCHAR(255)
-);
--- EVENTOS -- (CALENDARIO)-------------------------------------------------------
-CREATE TABLE eventos (
-    id_evento INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(50),
-    descripcion VARCHAR(100),
-    fecha DATE,
-    hora TIME
-);
+-- --------------------------------------------------------
 
---ENCUESTAS-------------------------------------------------------
+--
+-- Estructura de tabla para la tabla `contactos`
+--
 
--- ENCUESTAS --
-CREATE TABLE encuestas (
-    id_encuesta INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(50)
-);
+CREATE TABLE `contactos` (
+  `id_contactos` int(11) NOT NULL,
+  `nombre_institucion` text DEFAULT NULL,
+  `tipo_contacto` enum('telefono','email') DEFAULT NULL,
+  `contacto` text DEFAULT NULL,
+  `horario` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- PREGUNTAS --
-CREATE TABLE preguntas (
-    id_pregunta INT AUTO_INCREMENT PRIMARY KEY,
-    id_encuesta INT,
-    pregunta VARCHAR(100),
-    pregunta_abierta ENUM('0','1') DEFAULT '0',
-    pregunta_cerrada_multiple ENUM('0','1') DEFAULT '0',
-    FOREIGN KEY (id_encuesta) REFERENCES encuestas(id_encuesta)
-);
+-- --------------------------------------------------------
 
--- OPCIONES --
-CREATE TABLE opcion (
-    id_opcion INT AUTO_INCREMENT PRIMARY KEY,
-    id_pregunta INT,
-    id_encuesta INT,
-    opcion VARCHAR(100),
-    FOREIGN KEY (id_pregunta) REFERENCES preguntas(id_pregunta),
-    FOREIGN KEY (id_encuesta) REFERENCES encuestas(id_encuesta)
-);
+--
+-- Estructura de tabla para la tabla `docs_ley_general`
+--
 
--- RESPUESTAS --
+CREATE TABLE `docs_ley_general` (
+  `id_documento` int(11) NOT NULL,
+  `articulo` enum('69','70') DEFAULT NULL,
+  `fraccion` varchar(100) DEFAULT NULL,
+  `año` int(11) DEFAULT NULL,
+  `trimestre` enum('1','2','3','4') DEFAULT NULL,
+  `ruta_archivo` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- RESPUESTAS MULTIPLES --
-CREATE TABLE respuesta_cerrada (
-    id_respuesta INT AUTO_INCREMENT PRIMARY KEY,
-    id_opcion INT,
-    id_pregunta INT,
-    id_encuesta INT,
-    FOREIGN KEY (id_opcion) REFERENCES opcion(id_opcion),
-    FOREIGN KEY (id_pregunta) REFERENCES preguntas(id_pregunta),
-    FOREIGN KEY (id_encuesta) REFERENCES encuestas(id_encuesta)
-);
+-- --------------------------------------------------------
 
--- -- RESPUESTAS SELECCION MULTIPLE -- 
--- CREATE TABLE respuesta_multiple_varios (
---     id_respuesta INT AUTO_INCREMENT PRIMARY KEY,
---     id_opcion INT,
---     id_pregunta INT,
---     id_encuesta INT,
---     FOREIGN KEY (id_opcion) REFERENCES opcion(id_opcion),
---     FOREIGN KEY (id_pregunta) REFERENCES preguntas(id_pregunta),
---     FOREIGN KEY (id_encuesta) REFERENCES encuestas(id_encuesta)
--- );
+--
+-- Estructura de tabla para la tabla `encuestas`
+--
 
--- RESPUESTAS ABIERTAS --
-CREATE TABLE respuesta_abierta (
-    id_respuesta_abierta INT AUTO_INCREMENT PRIMARY KEY,
-    id_pregunta INT,
-    id_encuesta INT,
-    respuesta VARCHAR(200),
-    FOREIGN KEY (id_opcion) REFERENCES opcion(id_opcion),
-    FOREIGN KEY (id_pregunta) REFERENCES preguntas(id_pregunta),
-    FOREIGN KEY (id_encuesta) REFERENCES encuestas(id_encuesta)
-);
+CREATE TABLE `encuestas` (
+  `id_encuesta` int(11) NOT NULL,
+  `titulo` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- CHATBOT -------------------------------------------------------------------
-CREATE TABLE bot (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre TEXT,
-    correo TEXT,
-    problema TEXT,
-    area TEXT
-);
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `eventos`
+--
 
--- DOCUMENTOS LEY GENERAL DE TRANSPARENCIA ------------------------------------
-CREATE TABLE docs_ley_general (
-    id_documento INT AUTO_INCREMENT PRIMARY KEY,
-    articulo ENUM('69','70'),
-    fraccion VARCHAR(100),
-    año INT,
-    trimestre ENUM('1', '2', '3', '4'),
-    ruta_archivo VARCHAR(50)
-);
+CREATE TABLE `eventos` (
+  `id_evento` int(11) NOT NULL,
+  `titulo` varchar(50) DEFAULT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `header`
+--
 
--- Falta corregir tabla
--- usuarios (area asignada)(administrador si es para el admin)
--- probar si la logica de los docs funciona
--- probar si la logica de las encuestas funciona
--- hacer una nueva tabla para colores de la pagina 
--- hacer nueva pagina para banco de respuestas
--- modificar noticias para la ruta de las imagenes 
+CREATE TABLE `header` (
+  `id_header` int(11) NOT NULL,
+  `imagen` varchar(255) NOT NULL,
+  `ruta` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `header`
+--
+
+INSERT INTO `header` (`id_header`, `imagen`, `ruta`) VALUES
+(1, 'default_header.png', 'static/images/header/');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `logo`
+--
+
+CREATE TABLE `logo` (
+  `id_logo` int(11) NOT NULL,
+  `imagen` varchar(255) NOT NULL,
+  `ruta` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `logo`
+--
+
+INSERT INTO `logo` (`id_logo`, `imagen`, `ruta`) VALUES
+(1, 'default_icon.png', 'static/images/logo/');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `noticias`
+--
+
+CREATE TABLE `noticias` (
+  `id_noticia` int(11) NOT NULL,
+  `titulo` varchar(100) DEFAULT NULL,
+  `resumen` text DEFAULT NULL,
+  `contenido` longtext DEFAULT NULL,
+  `imagen` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `opcion`
+--
+
+CREATE TABLE `opcion` (
+  `id_opcion` int(11) NOT NULL,
+  `id_pregunta` int(11) DEFAULT NULL,
+  `id_encuesta` int(11) DEFAULT NULL,
+  `opcion` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `preguntas`
+--
+
+CREATE TABLE `preguntas` (
+  `id_pregunta` int(11) NOT NULL,
+  `id_encuesta` int(11) DEFAULT NULL,
+  `pregunta` varchar(100) DEFAULT NULL,
+  `pregunta_abierta` enum('0','1') DEFAULT '0',
+  `pregunta_cerrada_multiple` enum('0','1') DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuesta_abierta`
+--
+
+CREATE TABLE `respuesta_abierta` (
+  `id_respuesta_abierta` int(11) NOT NULL,
+  `id_pregunta` int(11) DEFAULT NULL,
+  `id_encuesta` int(11) DEFAULT NULL,
+  `respuesta` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuesta_cerrada`
+--
+
+CREATE TABLE `respuesta_cerrada` (
+  `id_respuesta` int(11) NOT NULL,
+  `id_opcion` int(11) DEFAULT NULL,
+  `id_pregunta` int(11) DEFAULT NULL,
+  `id_encuesta` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ubicaciones`
+--
+
+CREATE TABLE `ubicaciones` (
+  `id_ubicacion` int(11) NOT NULL,
+  `latitud` decimal(10,8) DEFAULT NULL,
+  `longitud` decimal(11,8) DEFAULT NULL,
+  `lugar` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `contrasena` varchar(255) NOT NULL,
+  `area` varchar(70) NOT NULL,
+  `estado` enum('0','1') DEFAULT '1',
+  `permisos` enum('0','1') DEFAULT '0',
+  `salt` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `bot`
+--
+ALTER TABLE `bot`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `carrusel`
+--
+ALTER TABLE `carrusel`
+  ADD PRIMARY KEY (`id_imagen`);
+
+--
+-- Indices de la tabla `contactos`
+--
+ALTER TABLE `contactos`
+  ADD PRIMARY KEY (`id_contactos`);
+
+--
+-- Indices de la tabla `docs_ley_general`
+--
+ALTER TABLE `docs_ley_general`
+  ADD PRIMARY KEY (`id_documento`);
+
+--
+-- Indices de la tabla `encuestas`
+--
+ALTER TABLE `encuestas`
+  ADD PRIMARY KEY (`id_encuesta`);
+
+--
+-- Indices de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`id_evento`);
+
+--
+-- Indices de la tabla `header`
+--
+ALTER TABLE `header`
+  ADD PRIMARY KEY (`id_header`);
+
+--
+-- Indices de la tabla `logo`
+--
+ALTER TABLE `logo`
+  ADD PRIMARY KEY (`id_logo`);
+
+--
+-- Indices de la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  ADD PRIMARY KEY (`id_noticia`);
+
+--
+-- Indices de la tabla `opcion`
+--
+ALTER TABLE `opcion`
+  ADD PRIMARY KEY (`id_opcion`),
+  ADD KEY `id_pregunta` (`id_pregunta`),
+  ADD KEY `id_encuesta` (`id_encuesta`);
+
+--
+-- Indices de la tabla `preguntas`
+--
+ALTER TABLE `preguntas`
+  ADD PRIMARY KEY (`id_pregunta`),
+  ADD KEY `id_encuesta` (`id_encuesta`);
+
+--
+-- Indices de la tabla `respuesta_abierta`
+--
+ALTER TABLE `respuesta_abierta`
+  ADD PRIMARY KEY (`id_respuesta_abierta`),
+  ADD KEY `id_pregunta` (`id_pregunta`),
+  ADD KEY `id_encuesta` (`id_encuesta`);
+
+--
+-- Indices de la tabla `respuesta_cerrada`
+--
+ALTER TABLE `respuesta_cerrada`
+  ADD PRIMARY KEY (`id_respuesta`),
+  ADD KEY `id_opcion` (`id_opcion`),
+  ADD KEY `id_pregunta` (`id_pregunta`),
+  ADD KEY `id_encuesta` (`id_encuesta`);
+
+--
+-- Indices de la tabla `ubicaciones`
+--
+ALTER TABLE `ubicaciones`
+  ADD PRIMARY KEY (`id_ubicacion`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `bot`
+--
+ALTER TABLE `bot`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `carrusel`
+--
+ALTER TABLE `carrusel`
+  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `contactos`
+--
+ALTER TABLE `contactos`
+  MODIFY `id_contactos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `docs_ley_general`
+--
+ALTER TABLE `docs_ley_general`
+  MODIFY `id_documento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `encuestas`
+--
+ALTER TABLE `encuestas`
+  MODIFY `id_encuesta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  MODIFY `id_evento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `header`
+--
+ALTER TABLE `header`
+  MODIFY `id_header` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `logo`
+--
+ALTER TABLE `logo`
+  MODIFY `id_logo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  MODIFY `id_noticia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `opcion`
+--
+ALTER TABLE `opcion`
+  MODIFY `id_opcion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `preguntas`
+--
+ALTER TABLE `preguntas`
+  MODIFY `id_pregunta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `respuesta_abierta`
+--
+ALTER TABLE `respuesta_abierta`
+  MODIFY `id_respuesta_abierta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `respuesta_cerrada`
+--
+ALTER TABLE `respuesta_cerrada`
+  MODIFY `id_respuesta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ubicaciones`
+--
+ALTER TABLE `ubicaciones`
+  MODIFY `id_ubicacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `opcion`
+--
+ALTER TABLE `opcion`
+  ADD CONSTRAINT `opcion_ibfk_1` FOREIGN KEY (`id_pregunta`) REFERENCES `preguntas` (`id_pregunta`),
+  ADD CONSTRAINT `opcion_ibfk_2` FOREIGN KEY (`id_encuesta`) REFERENCES `encuestas` (`id_encuesta`);
+
+--
+-- Filtros para la tabla `preguntas`
+--
+ALTER TABLE `preguntas`
+  ADD CONSTRAINT `preguntas_ibfk_1` FOREIGN KEY (`id_encuesta`) REFERENCES `encuestas` (`id_encuesta`);
+
+--
+-- Filtros para la tabla `respuesta_abierta`
+--
+ALTER TABLE `respuesta_abierta`
+  ADD CONSTRAINT `respuesta_abierta_ibfk_1` FOREIGN KEY (`id_pregunta`) REFERENCES `preguntas` (`id_pregunta`),
+  ADD CONSTRAINT `respuesta_abierta_ibfk_2` FOREIGN KEY (`id_encuesta`) REFERENCES `encuestas` (`id_encuesta`);
+
+--
+-- Filtros para la tabla `respuesta_cerrada`
+--
+ALTER TABLE `respuesta_cerrada`
+  ADD CONSTRAINT `respuesta_cerrada_ibfk_1` FOREIGN KEY (`id_opcion`) REFERENCES `opcion` (`id_opcion`),
+  ADD CONSTRAINT `respuesta_cerrada_ibfk_2` FOREIGN KEY (`id_pregunta`) REFERENCES `preguntas` (`id_pregunta`),
+  ADD CONSTRAINT `respuesta_cerrada_ibfk_3` FOREIGN KEY (`id_encuesta`) REFERENCES `encuestas` (`id_encuesta`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ script.sql
@@ -0,0 +1,152 @@
+CREATE DATABASE presidencia;
+USE presidencia;
+
+
+-- USUARIOS (DONE) -------------------------------------------------------
+CREATE TABLE usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL UNIQUE,
+    contrasena VARCHAR(255) NOT NULL,
+    area VARCHAR(70) NOT NULL,
+    estado ENUM('0', '1') DEFAULT '1',
+    permisos ENUM('0', '1') DEFAULT '0',
+    salt VARCHAR(255) NOT NULL
+);
+
+-- PAGINA ---------------------------------------------------------
+CREATE TABLE logo (
+    id_logo INT AUTO_INCREMENT PRIMARY KEY,
+    imagen VARCHAR(255) NOT NULL,
+    ruta VARCHAR(255) NOT NULL
+);
+
+INSERT INTO logo (imagen,ruta) VALUES
+('default_icon.png','static/images/logo/');
+
+CREATE TABLE header (
+    id_header INT AUTO_INCREMENT PRIMARY KEY,
+    imagen VARCHAR(255) NOT NULL,
+    ruta VARCHAR(255) NOT NULL
+);
+
+INSERT INTO header (imagen,ruta) VALUES
+('default_header.png','static/images/header/');
+
+CREATE TABLE carrusel (
+    id_imagen INT AUTO_INCREMENT PRIMARY KEY,
+    imagen VARCHAR(255) NOT NULL,
+    ruta VARCHAR(255) NOT NULL,
+    estado ENUM('0', '1') DEFAULT '1',
+    url VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE ubicaciones (
+    id_ubicacion INT AUTO_INCREMENT PRIMARY KEY,
+    latitud DECIMAL(10, 8),
+    longitud DECIMAL(11, 8),
+    lugar VARCHAR(255)
+);
+
+CREATE TABLE contactos (
+    id_contactos INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_institucion TEXT,
+    tipo_contacto ENUM('telefono','email'),
+    contacto TEXT,
+    horario TEXT
+);
+
+-- NOTICIAS ---------------------------------------------------------
+CREATE TABLE noticias (
+    id_noticia INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(100),
+    resumen TEXT,
+    contenido longtext,
+    imagen VARCHAR(255)
+);
+-- EVENTOS -- (CALENDARIO)-------------------------------------------------------
+CREATE TABLE eventos (
+    id_evento INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(50),
+    descripcion VARCHAR(100),
+    fecha DATE,
+    hora TIME
+);
+
+-- ENCUESTAS --
+CREATE TABLE encuestas (
+    id_encuesta INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(50)
+);
+
+-- PREGUNTAS --
+CREATE TABLE preguntas (
+    id_pregunta INT AUTO_INCREMENT PRIMARY KEY,
+    id_encuesta INT,
+    pregunta VARCHAR(100),
+    pregunta_abierta ENUM('0','1') DEFAULT '0',
+    pregunta_cerrada_multiple ENUM('0','1') DEFAULT '0',
+    FOREIGN KEY (id_encuesta) REFERENCES encuestas(id_encuesta)
+);
+
+-- OPCIONES --
+CREATE TABLE opcion (
+    id_opcion INT AUTO_INCREMENT PRIMARY KEY,
+    id_pregunta INT,
+    id_encuesta INT,
+    opcion VARCHAR(100),
+    FOREIGN KEY (id_pregunta) REFERENCES preguntas(id_pregunta),
+    FOREIGN KEY (id_encuesta) REFERENCES encuestas(id_encuesta)
+);
+
+-- RESPUESTAS --
+
+-- RESPUESTAS MULTIPLES --
+CREATE TABLE respuesta_cerrada (
+    id_respuesta INT AUTO_INCREMENT PRIMARY KEY,
+    id_opcion INT,
+    id_pregunta INT,
+    id_encuesta INT,
+    FOREIGN KEY (id_opcion) REFERENCES opcion(id_opcion),
+    FOREIGN KEY (id_pregunta) REFERENCES preguntas(id_pregunta),
+    FOREIGN KEY (id_encuesta) REFERENCES encuestas(id_encuesta)
+);
+
+-- RESPUESTAS ABIERTAS --
+CREATE TABLE respuesta_abierta (
+    id_respuesta_abierta INT AUTO_INCREMENT PRIMARY KEY,
+    id_pregunta INT,
+    id_encuesta INT,
+    respuesta VARCHAR(200),
+    FOREIGN KEY (id_pregunta) REFERENCES preguntas(id_pregunta),
+    FOREIGN KEY (id_encuesta) REFERENCES encuestas(id_encuesta)
+);
+
+-- CHATBOT -------------------------------------------------------------------
+CREATE TABLE bot (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre TEXT,
+    correo TEXT,
+    problema TEXT,
+    area TEXT
+);
+
+
+-- DOCUMENTOS LEY GENERAL DE TRANSPARENCIA ------------------------------------
+CREATE TABLE docs_ley_general (
+    id_documento INT AUTO_INCREMENT PRIMARY KEY,
+    articulo ENUM('69','70'),
+    fraccion VARCHAR(100),
+    año INT,
+    trimestre ENUM('1', '2', '3', '4'),
+    ruta_archivo VARCHAR(50)
+);
+
+
+
+-- Falta corregir tabla
+-- usuarios (area asignada)(administrador si es para el admin)
+-- probar si la logica de los docs funciona
+-- probar si la logica de las encuestas funciona
+-- hacer una nueva tabla para colores de la pagina 
+-- hacer nueva pagina para banco de respuestas
+-- modificar noticias para la ruta de las imagenes 
